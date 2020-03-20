06564ca2bf61c79e358fbb87aa6bfba13b3bf404
Arun <arun@gocoop.com>
2020-03-20 11:20:15 +0530

    Project task exception and findbyId

--- ppmtool/src/main/java/io/hastentech/ppmtool/service/impl/ProjectServiceImpl.java
+++ ppmtool/src/main/java/io/hastentech/ppmtool/service/impl/ProjectServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.hastentech.ppmtool.domain.Project;
+import io.hastentech.ppmtool.exceptions.ProjectIdException;
 import io.hastentech.ppmtool.repository.ProjectRepository;
 import io.hastentech.ppmtool.service.ProjectService;
 
@@ -16,8 +17,24 @@ public class ProjectServiceImpl implements ProjectService{
 	@Override
 	public Project saveOrUpdateProject(Project project) {
 		// TODO Auto-generated method stub
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch (Exception e) {
+			// TODO: handle exception
+			throw new ProjectIdException("Project Id "+project.getProjectIdentifier().toUpperCase()+" is already exixst");
+		}
+		 
+	}
+
+	@Override
+	public Project getProjectByIdentifier(String projectId) {
 		
-		return projectRepository.save(project); 
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if(project == null) {
+			throw new ProjectIdException("Project Id "+projectId+" is not exists");
+		}
+		return project;
 	}
 
 }
