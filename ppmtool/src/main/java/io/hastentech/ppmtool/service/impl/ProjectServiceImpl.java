7a64106080fbbcffa07040fb5bac3891a73e1c38
Arun <arun@gocoop.com>
2020-03-21 11:43:01 +0530

    new commit with java and react

--- ppmtool/src/main/java/io/hastentech/ppmtool/service/impl/ProjectServiceImpl.java
+++ ppmtool/src/main/java/io/hastentech/ppmtool/service/impl/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package io.hastentech.ppmtool.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import io.hastentech.ppmtool.domain.Project;
@@ -35,6 +36,24 @@ public class ProjectServiceImpl implements ProjectService{
 			throw new ProjectIdException("Project Id "+projectId+" is not exists");
 		}
 		return project;
+	}
+
+	@Override
+	public Iterable<Project> findAllProjects() {
+		// TODO Auto-generated method stub
+		return projectRepository.findAll();
+	}
+
+	@Override
+	public void deleteProjectByIdentifier(String projectId) {
+		// TODO Auto-generated method stub
+		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if(project == null) {
+			throw new ProjectIdException("Project with id '"+projectId+"' does not exist");
+		}
+		
+		projectRepository.delete(project);
 	}
 
 }
